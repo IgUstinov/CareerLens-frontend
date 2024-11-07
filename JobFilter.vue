c1220f25a9e6cdfb1bf5bf64e9236c977244357e
Igor <108358448+IgUstinov@users.noreply.github.com>
2024-11-08 00:44:08 +0400

    -

---
+++ JobFilter.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <label>Region:</label>
+      <input v-model="region" placeholder="Enter region" />
+  
+      <label>Salary:</label>
+      <input v-model="salary" type="number" placeholder="Minimum salary" />
+  
+      <label>Experience:</label>
+      <select v-model="experience">
+        <option value="">Any</option>
+        <option value="Junior">Junior</option>
+        <option value="Mid">Mid</option>
+        <option value="Senior">Senior</option>
+      </select>
+  
+      <button @click="applyFilter">Apply Filter</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        region: '',
+        salary: null,
+        experience: '',
+      };
+    },
+    methods: {
+      applyFilter() {
+        this.$emit('applyFilter', {
+          region: this.region,
+          salary: this.salary ? { $gte: this.salary } : null,
+          experience: this.experience || null,
+        });
+      },
+    },
+  };
+  </script>
+  
---
+++ JobList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <JobCard v-for="job in jobs" :key="job.id" :job="job" />
+    </div>
+  </template>
+  
+  <script>
+  import JobCard from './JobCard.vue';
+  
+  export default {
+    props: ['jobs'],
+    components: { JobCard },
+  };
+  </script>
+  
